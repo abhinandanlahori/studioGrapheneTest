eb745ca1967bc2b7916e189bdf62b696ba974ce8
Abhinandan Lahori <abhinandanlahori@gmail.com>
2022-11-14 11:52:11 +0530

    optimized the code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import NavBar from './NavBar';
+import React from 'react'
+import menu from 'components'
+import navbar from 'components'
 
-function Navigation() {
-  return (
-    <div className="navPage">
-
-    <NavBar />
-
-    <img id="navBackground" src="../bg-pic.png" alt="navBackground" />
-    <img id="navLogo" src="../stamp.png" alt="logo" />
-
-      <div className="navText">
-
-        <h2 className="white">THE BEST FOODIE</h2>
-
-        <div>
-          <h1 className="white">EXPERIENCE</h1>
-        </div>
-
-        <div>
-          <h3 className="white">NOW IN LONDON</h3>
-        </div>
-        
-        
-      </div>
-      
-    </div>
-  );
+function home(){
+     user_new = [n,n+1]
+     AuthenticatorAssertionResponse.user_new() == 1;
+     PermissionStatus.name;
 }
 
-export default Navigation;
+if (n=1, n+1, n+2, n+3){
+ FileSystem.log("Try again after 30 seconds");
+ FileSystem.log("Try again after 60 seconds");
+ FileSystem.log("Try again after 90 seconds");
+}
+
+import { JSXElementConstructor } from 'react';
+
+let main = {
+    users: "$user_name"
+    [
+        {
+              name: "$name"
+        }
+    ]
+}
+function Navigation() {
+    return (
+      <div className="navPage">
+  
+      <NavBar />
+  
+      <img id="navBackground" src="../bg-pic.png" alt="navBackground" />
+      <img id="navLogo" src="../stamp.png" alt="logo" />
+  
+        <div className="navText">
+  
+          <h2 className="white">THE BEST FOODIE</h2>
+  
+          <div>
+            <h1 className="white">EXPERIENCE</h1>
+          </div>
+  
+          <div>
+            <h3 className="white">NOW IN LONDON</h3>
+          </div>
+          
+          
+        </div>
+        
+      </div>
+    );
+  }
